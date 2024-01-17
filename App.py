91255b3708b760b4ce6489f46f5d5de72c7841c7
Abdulrahman KT <113695490+abduuee@users.noreply.github.com>
2024-01-18 00:18:30 +0400

    completed

--- App.py
+++ App.py
@@ -18,7 +18,7 @@ tmdb.api_key = '6009b11afc8bcef52fe3d9b47ad03d7e'
 logo_img = None
 
 def home_page():
-    home_frame = Frame(main_frame, bg="#A9B1A7")
+    home_frame = Frame(body_screen, bg="#A9B1A7")
 
     def display_logo():
         global logo_img
@@ -38,7 +38,7 @@ def home_page():
     
 def search_page():
     search_results = []
-    current_result_index = 0
+    crn_index_result = 0
 
     # Function to get the user's query and provide results
     def search_movie():
@@ -50,12 +50,8 @@ def search_page():
             search_results = movie.search(query)
 
             # To display the number of results
-            result_label.config(text=f"Results: {current_result_index + 1} of {len(search_results)}")
-            
-            details_text.config(state=NORMAL)
-            details_text.delete(1.0, END)
-            details_text.config(state=DISABLED)
-
+            times_amount.config(text=f"Results: {crn_index_result + 1} of {len(search_results)}")
+        
             if search_results:
                 movie_id = search_results[0].id
                 display_movie_details(movie_id)
@@ -63,7 +59,7 @@ def search_page():
 
     # Function to load and display movie details
     def display_movie_details(movie_id):
-        nonlocal current_result_index
+        nonlocal crn_index_result
         movie = Movie()
         selected_movie = movie.details(movie_id)
 
@@ -92,15 +88,15 @@ def search_page():
 
     # Function to load and display movie poster
     def load_and_display_poster(movie_id):
-        nonlocal current_result_index
+        nonlocal crn_index_result
         movie = Movie()
         selected_movie = movie.details(movie_id)
-        poster_path = selected_movie.poster_path
+        movie_image = selected_movie.poster_path
 
-        if poster_path:
-            poster_url = f"https://image.tmdb.org/t/p/w500/{poster_path}"
-            response = requests.get(poster_url)
-            img = Image.open(io.BytesIO(response.content))
+        if movie_image:
+            poster_url = f"https://image.tmdb.org/t/p/w500/{movie_image}"
+            pick = requests.get(poster_url)
+            img = Image.open(io.BytesIO(pick.content))
             img = img.resize((240, 370), Image.BICUBIC)
             mask = Image.new("L", img.size, 0)
             draw = ImageDraw.Draw(mask)
@@ -108,25 +104,24 @@ def search_page():
             draw.rounded_rectangle([(0, 0), img.size], radius, fill=255)
             img.putalpha(mask)
             img = ImageTk.PhotoImage(img)
-            poster_label.config(image=img)
-            poster_label.image = img
+            movie_area.config(image=img)
+            movie_area.image = img
 
         else:
-            poster_label.config(image="")
-            poster_label.image = None
+            movie_area.config(image="")
+            movie_area.image = None
 
     # Function to add a background image to the page
     def set_background():
         background_img = Image.open("Images/Search_bg.png")
-        new_size = (main_frame.winfo_reqwidth(), main_frame.winfo_reqheight())
+        new_size = (body_screen.winfo_reqwidth(), body_screen.winfo_reqheight())
         background_img = background_img.resize(new_size, Image.BICUBIC)
         background_img = ImageTk.PhotoImage(background_img)
-
         canvas.grid(row=0, column=0, rowspan=6, columnspan=6, sticky="nsew")
         canvas.create_image(0, 0, anchor=NW, image=background_img)
         canvas.background = background_img
 
-    search_frame = Frame(main_frame, bg="#A9B1A7")
+    search_frame = Frame(body_screen, bg="#A9B1A7")
     search_frame.pack(side=LEFT)
     search_frame.pack_propagate(False)
     search_frame.configure(width=710, height=665)
@@ -134,8 +129,8 @@ def search_page():
     set_background()
 
     # Movie Poster Area
-    poster_label = Label(search_frame, text="Poster will be displayed here", font=("Montserrat", 10), bg="#A9B1A7")
-    poster_label.grid(row=1, column=0, rowspan=3, padx=10, pady=5)
+    movie_area = Label(search_frame, text="Poster will be displayed here", font=("Montserrat", 10), bg="#A9B1A7")
+    movie_area.grid(row=1, column=0, rowspan=3, padx=10, pady=5)
 
     # Adding empty columns to center the widgets
     search_frame.columnconfigure(1, weight=1)
@@ -144,14 +139,14 @@ def search_page():
 
     # Search Entry and Button
     search_entry = Entry(search_frame, font=("Montserrat", 15), width=30)
-    search_entry.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+    search_entry.grid(row=0, column=1, padx=10, pady=10)
 
     search_button = Button(search_frame, text="Search", font=("Montserrat", 13), width=10, command=search_movie, bg="#697664", fg="#EAEBE9")
-    search_button.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
+    search_button.grid(row=0, column=2, padx=10, pady=10)
 
     # Results Label
-    result_label = Label(search_frame, text="", font=("Montserrat", 12), bg="#A9B1A7")
-    result_label.grid(row=1, column=1, columnspan=3, pady=5)
+    times_amount = Label(search_frame, text="", font=("Montserrat", 12), bg="#A9B1A7")
+    times_amount.grid(row=1, column=1, columnspan=3, pady=5)
 
     # Movie Details Area
     details_text = Text(search_frame, wrap=WORD, width=50, height=25, borderwidth=0, highlightbackground="#697664", highlightthickness=1, bg="#A9B1A7")
@@ -164,20 +159,20 @@ def search_page():
     next_button.grid(row=3, column=1, padx=15, pady=5)
 
     def next_result():
-        nonlocal current_result_index
-        if current_result_index < len(search_results) - 1:
-            current_result_index += 1
-            display_movie_details(search_results[current_result_index].id)
-            load_and_display_poster(search_results[current_result_index].id)
-            result_label.config(text=f"Results: {current_result_index + 1} of {len(search_results)}")
+        nonlocal crn_index_result
+        if crn_index_result < len(search_results) - 1:
+            crn_index_result += 1
+            display_movie_details(search_results[crn_index_result].id)
+            load_and_display_poster(search_results[crn_index_result].id)
+            times_amount.config(text=f"Results: {crn_index_result + 1} of {len(search_results)}")
 
     def prev_result():
-        nonlocal current_result_index
-        if current_result_index > 0:
-            current_result_index -= 1
-            display_movie_details(search_results[current_result_index].id)
-            load_and_display_poster(search_results[current_result_index].id)
-            result_label.config(text=f"Results: {current_result_index + 1} of {len(search_results)}")
+        nonlocal crn_index_result
+        if crn_index_result > 0:
+            crn_index_result -= 1
+            display_movie_details(search_results[crn_index_result].id)
+            load_and_display_poster(search_results[crn_index_result].id)
+            times_amount.config(text=f"Results: {crn_index_result + 1} of {len(search_results)}")
 
     search_frame.pack(pady=20)
 
@@ -186,20 +181,20 @@ def search_page():
 
 # Function to display movie posters for the list of movies
 def display_poster(poster_url, title, row_index, col_index):
-    response = requests.get(poster_url)
+    pick = requests.get(poster_url)
 
-    if response.status_code == 200:
-        img = Image.open(io.BytesIO(response.content))
+    if pick.status_code == 200:
+        img = Image.open(io.BytesIO(pick.content))
         img = img.resize((150, 250), Image.BICUBIC)
         photo = ImageTk.PhotoImage(img)
-        poster_label = Label(main_frame, image=photo)
-        poster_label.grid(row=row_index * 2, column=col_index, padx=10, pady=10)
-        title_label = Label(main_frame, text=title, font=("Montserrat", 13), bg="#A9B1A7")
+        movie_info = Label(body_screen, image=photo)
+        movie_info.grid(row=row_index * 2, column=col_index, padx=10, pady=10)
+        title_label = Label(body_screen, text=title, font=("Montserrat", 13), bg="#A9B1A7")
         title_label.grid(row=row_index * 2 + 1, column=col_index, padx=10, pady=5)
-        poster_label.image = photo
+        movie_info.image = photo
 
     else:
-        Label(main_frame, text="Poster not available").grid(row=row_index * 2, column=col_index, padx=10, pady=10)
+        Label(body_screen, text="Poster not available").grid(row=row_index * 2, column=col_index, padx=10, pady=10)
 
 # Function to load trending movies list
 def load_trending():
@@ -214,15 +209,15 @@ def load_trending():
     if trending_movies:
         row_index = 1
         col_index = 0
-        trending_heading = Label(main_frame, text="Trending Movies:", font=("Montserrat", 15, "bold"), bg="#A9B1A7")
+        trending_heading = Label(body_screen, text="Trending Movies:", font=("Montserrat", 15, "bold"), bg="#A9B1A7")
         trending_heading.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
         for movie in trending_movies:
             title = movie.title
-            poster_path = movie.poster_path
+            movie_img = movie.poster_path
 
-            if poster_path:
-                poster_url = f"https://image.tmdb.org/t/p/w500/{poster_path}"
+            if movie_img:
+                poster_url = f"https://image.tmdb.org/t/p/w500/{movie_img}"
                 display_poster(poster_url, title, row_index, col_index)
 
             col_index += 1
@@ -233,7 +228,7 @@ def load_trending():
                     break
 
 def trending_page():
-    trending_frame = Frame(main_frame)
+    trending_frame = Frame(body_screen)
     load_trending()
 
     trending_frame.pack(pady=20)
@@ -243,20 +238,20 @@ def trending_page():
     
 # Function to display movie posters for the list of movies
 def display_top_rated_poster(poster_url, title, row_index, col_index):
-    response = requests.get(poster_url)
+    pick = requests.get(poster_url)
 
-    if response.status_code == 200:
-        img = Image.open(io.BytesIO(response.content))
+    if pick.status_code == 200:
+        img = Image.open(io.BytesIO(pick.content))
         img = img.resize((150, 250), Image.BICUBIC)
         photo = ImageTk.PhotoImage(img)
-        poster_label = Label(main_frame, image=photo)
-        poster_label.grid(row=row_index * 2, column=col_index, padx=10, pady=10)
-        title_label = Label(main_frame, text=title, font=("Montserrat", 11), bg="#A9B1A7")
+        movie_info = Label(body_screen, image=photo)
+        movie_info.grid(row=row_index * 2, column=col_index, padx=10, pady=10)
+        title_label = Label(body_screen, text=title, font=("Montserrat", 11), bg="#A9B1A7")
         title_label.grid(row=row_index * 2 + 1, column=col_index, padx=10, pady=5)
-        poster_label.image = photo
+        movie_info.image = photo
 
     else:
-        Label(main_frame, text="Poster not available").grid(row=row_index * 2, column=col_index, padx=10, pady=10)
+        Label(body_screen, text="Poster not available").grid(row=row_index * 2, column=col_index, padx=10, pady=10)
 
 # Function to load top rated movies list
 def load_top_rated():
@@ -271,15 +266,15 @@ def load_top_rated():
     if top_rated_movies:
         row_index = 1
         col_index = 0
-        toprated_heading = Label(main_frame, text="Top Rated Movies:", font=("Montserrat", 15, "bold"), bg="#A9B1A7")
+        toprated_heading = Label(body_screen, text="Top Rated Movies:", font=("Montserrat", 15, "bold"), bg="#A9B1A7")
         toprated_heading.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
         for movie in top_rated_movies:
             title = movie.title
-            poster_path = movie.poster_path
+            movie_img = movie.poster_path
 
-            if poster_path:
-                poster_url = f"https://image.tmdb.org/t/p/w500/{poster_path}"
+            if movie_img:
+                poster_url = f"https://image.tmdb.org/t/p/w500/{movie_img}"
                 display_top_rated_poster(poster_url, title, row_index, col_index)
 
             col_index += 1
@@ -291,7 +286,7 @@ def load_top_rated():
 
 def toprated_page():
     global toprated_frame
-    toprated_frame = Frame(main_frame)
+    toprated_frame = Frame(body_screen)
     load_top_rated()
 
     toprated_frame.pack(pady=20)
@@ -300,25 +295,25 @@ def toprated_page():
 # Navigation Menu
 
 # Function to erase the pages already viewed
-def delete_pages():
-    for frame in main_frame.winfo_children():
+def clear_screens():
+    for frame in body_screen.winfo_children():
         frame.destroy()
 
 # Function to hide the indicators for the navigation menu
-def hide_indicators():
-    home_indicate.config(bg="#283B22")
-    search_indicate.config(bg="#283B22")
-    trending_indicate.config(bg="#283B22")
-    toprated_indicate.config(bg="#283B22")
+def marking():
+    home_mark.config(bg="#283B22")
+    search_mark.config(bg="#283B22")
+    trending_mark.config(bg="#283B22")
+    toprated_mark.config(bg="#283B22")
 
 # Function to indicate for the navigation menu
-def indicate(lb, page):
-    hide_indicators()
+def mark(lb, page):
+    marking()
     lb.config(bg="#A9B1A7")
-    delete_pages()
+    clear_screens()
     page()
 
-options_frame = Frame(root, bg="#283B22")
+navigation = Frame(root, bg="#283B22")
 
 # Function to display the logo in the navigation menu
 def display_nav_logo():
@@ -326,8 +321,8 @@ def display_nav_logo():
     try:
         pil_image = Image.open("Images/nav_logo.png")
         pil_image = pil_image.resize((120, 130), Image.BICUBIC)
-        logo_img = ImageTk.PhotoImage(master=options_frame, image=pil_image)
-        logo_label = Label(options_frame, image=logo_img, borderwidth=0)
+        logo_img = ImageTk.PhotoImage(master=navigation, image=pil_image)
+        logo_label = Label(navigation, image=logo_img, borderwidth=0)
         logo_label.image = logo_img
         logo_label.place (y=15)
     except Exception as e:
@@ -336,42 +331,42 @@ def display_nav_logo():
 display_nav_logo()
 
 # Adding Labels, Buttons, and the Main Frame
-home_btn = Button(options_frame, text="Home", font=("Montserrat", 15), fg="#A9B1A7",
-                     bd=0, bg="#283B22", command=lambda: indicate(home_indicate, home_page))
+home_btn = Button(navigation, text="Home", font=("Montserrat", 15), fg="#A9B1A7",
+                     bd=0, bg="#283B22", command=lambda: mark(home_mark, home_page))
 home_btn.place(x=10, y=240)
 
-home_indicate = Label(options_frame, text='', bg="#283B22")
-home_indicate.place(x=3, y=240, width=5, height=40)
+home_mark = Label(navigation, text='', bg="#283B22")
+home_mark.place(x=3, y=240, width=5, height=40)
 
-search_btn = Button(options_frame, text="Search", font=("Montserrat", 15), fg="#A9B1A7",
-                     bd=0, bg="#283B22", command=lambda: indicate(search_indicate, search_page))
+search_btn = Button(navigation, text="Search", font=("Montserrat", 15), fg="#A9B1A7",
+                     bd=0, bg="#283B22", command=lambda: mark(search_mark, search_page))
 search_btn.place(x=10, y=290)
 
-search_indicate = Label(options_frame, text='', bg="#283B22")
-search_indicate.place(x=3, y=290, width=5, height=40)
+search_mark = Label(navigation, text='', bg="#283B22")
+search_mark.place(x=3, y=290, width=5, height=40)
 
-trending_btn = Button(options_frame, text="Trending", font=("Montserrat", 15), fg="#A9B1A7",
-                   bd=0, bg="#283B22", command=lambda: indicate(trending_indicate, trending_page))
+trending_btn = Button(navigation, text="Trending", font=("Montserrat", 15), fg="#A9B1A7",
+                   bd=0, bg="#283B22", command=lambda: mark(trending_mark, trending_page))
 trending_btn.place(x=10, y=340)
 
-trending_indicate = Label(options_frame, text='', bg="#283B22")
-trending_indicate.place(x=3, y=340, width=5, height=40)
+trending_mark = Label(navigation, text='', bg="#283B22")
+trending_mark.place(x=3, y=340, width=5, height=40)
 
-toprated_btn = Button(options_frame, text="Top Rated", font=("Montserrat", 15), fg="#A9B1A7",
-                       bd=0, bg="#283B22", command=lambda: indicate(toprated_indicate, toprated_page))
+toprated_btn = Button(navigation, text="Top Rated", font=("Montserrat", 15), fg="#A9B1A7",
+                       bd=0, bg="#283B22", command=lambda: mark(toprated_mark, toprated_page))
 toprated_btn.place(x=10, y=390)
 
-toprated_indicate = Label(options_frame, text='', bg="#283B22")
-toprated_indicate.place(x=3, y=390, width=5, height=40)
+toprated_mark = Label(navigation, text='', bg="#283B22")
+toprated_mark.place(x=3, y=390, width=5, height=40)
 
-options_frame.pack(side=LEFT)
-options_frame.pack_propagate(False)
-options_frame.configure(width=120, height=670)
+navigation.pack(side=LEFT)
+navigation.pack_propagate(False)
+navigation.configure(width=120, height=670)
 
-main_frame = Frame(root, highlightbackground="#A9B1A7", highlightthickness=2, bg="#A9B1A7")
-main_frame.pack(side=RIGHT)
-main_frame.pack_propagate(False)
-main_frame.configure(width=710, height=670)
+body_screen = Frame(root, highlightbackground="#A9B1A7", highlightthickness=3, bg="#A9B1A7")
+body_screen.pack(side=RIGHT)
+body_screen.pack_propagate(False)
+body_screen.configure(width=710, height=670)
 
 home_page()
 
